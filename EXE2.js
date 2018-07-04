ca8c6dc4c39fa46252f0caa6c014256f2977c9ad
Shem Bar <shem@svcollege.co.il>
2018-07-04 12:23:57 +0300

    EXE2

---
+++ EXE2.js
@@ -0,0 +1,89 @@
+{
+
+    var num = 10 ; 
+
+    if(num%2==0 && num>=0){
+        alert("Even and Positive");
+    }
+    else if(num%2==0 && num<0){
+        alert("Odd and Negative");
+    }
+    else if(num%2!=0 && num >=0 ){
+        alert("Even and positive");
+    }
+    else{
+        alert("Odd and Negative");
+    }
+
+    var num2 = -16;
+
+    if(num2>=0){
+        alert(num2);
+    }
+    else{
+        alert(num2*-1);
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function bla(){
+//     d=document.getElementById("demo").value;
+//     if(d>150){
+//         alert("noooooooo")
+//     }
+//     else{
+//         alert("yaaaaaaaa");
+//     }
+// }
+
+
+
+
+
+//}
+
+
+
